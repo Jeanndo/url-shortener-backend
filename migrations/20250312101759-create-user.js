21a714d47c9accb948abe9911ae40168adc4d80f
Jeanndo <ukjeando@gmail.com>
2025-03-12 12:20:40 +0200

    user model and migration

---
+++ migrations/20250312101759-create-user.js
@@ -0,0 +1,35 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      username: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable("users");
+  },
+};
